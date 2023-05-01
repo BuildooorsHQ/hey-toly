1d0f16505d8131aefb4f9c381144467d8e3c533a
Paras <nauriyalparas0@gmail.com>
2023-05-01 14:03:48 +0530

    Fixes Issue #1
    Increased max_token so that character limit by Chat-GPT in also increased

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -30,7 +30,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(toly),
       temperature: 0.6,
-      max_tokens: 256,
+      max_tokens: 500,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
