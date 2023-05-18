42fc886dcf298b77e7cc28de1b54d403ae950beb
johnforfar <john.forfar@gmail.com>
2023-05-18 19:11:56 +1000

    Simplified error handling

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,5 +1,5 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+import { HumanChatMessage, SystemChatMessage, AIChatMessage } from "langchain/schema";
 import saveToDb from "./saveToDb.js";
 
 function generatePrompt(toly) {
@@ -28,10 +28,12 @@ export default async function handler(req, res) {
  }
 
  try {
- const chat = new ChatOpenAI({ temperature: 0, apiKey: process.env.OPENAI_API_KEY, modelName: "gpt-3.5-turbo" });
+ const chat = new ChatOpenAI({ temperature: 0.9, apiKey: process.env.OPENAI_API_KEY, modelName: "gpt-3.5-turbo" });
  // Add some logging statements to check the input and options
  console.log([new SystemChatMessage("This is a technical FAQ chatbot that answers questions about Toly."), new HumanChatMessage(generatePrompt(toly))]);
- console.log({ temperature: 0.9, apiKey: process.env.OPENAI_API_KEY });
+
+ console.log({ temperature: chat.temperature, apiKey: process.env.OPENAI_API_KEY ? "VALID" : "INVALID" });
+
  const response = await chat.call([
  // Add a SystemChatMessage as the first message
  new SystemChatMessage("This is a technical FAQ chatbot that answers questions about Toly."),
@@ -39,51 +41,20 @@ export default async function handler(req, res) {
  ]);
 
  // Add a console log output of the response
- console.log(response);
+ console.log("API Response: ", response);
 
- // Check if the response is valid and has choices
- if (response && response.choices && response.choices.length > 0) {
- // Get the first choice and its message
- const choice = response.choices[0];
- const { message } = choice;
- 
- // Check if the message is valid and has content
- if (message && message.content) {
- // Remove escaped characters from the content
- const removeEscapedCharacters = (str) => str.replace(/\\n/g, '\n');
- const text = removeEscapedCharacters(message.content);
+ const { text } = response;
 
  // Send the text as the completion
  res.status(200).json({ completion: text });
  saveToDb("heytoly", "faqs", { toly, result: text });
- } else {
- // The message is invalid or has no content
- throw new Error("The message is invalid or has no content");
- }
- } else {
- // The response is invalid or has no choices
- throw new Error("The response is invalid or has no choices");
- }
- } catch (error) {
- // Error handling
- console.error(`Error with Langchain API request: ${error.message}`);
-
- if (error.response) {
- // The request was made and the API responded with an error
- console.error(`API response status: ${error.response.status}`);
- console.error(`API response data: ${JSON.stringify(error.response.data)}`);
- } else if (error.request) {
- // The request was made but no response was received
- console.error(`No response received from the API. Request details: ${error.request}`);
- } else {
- // Other types of errors
- console.error(`Error message: ${error.message}`);
- }
-
+} catch (error) {
+ // eslint-disable-next-line @typescript-eslint/no-explicit-any
+ console.error(error);
  res.status(500).json({
  error: {
- message: 'An error occurred during your request.',
+ message: "An error occurred",
  },
  });
- }
+}
 }
