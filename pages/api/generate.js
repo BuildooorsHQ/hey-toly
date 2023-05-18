021d6599e7c160de73e367948ebaca98294ee6bb
johnforfar <john.forfar@gmail.com>
2023-05-18 14:15:54 +1000

    Migration completed to Langchain

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,32 +1,16 @@
-import { Configuration, OpenAIApi } from "openai";
+import { ChatOpenAI, HumanChatMessage } from "langchain";
 import saveToDb from "./saveToDb.js";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
 
 function generatePrompt(toly) {
   const capitalizedToly = toly[0].toUpperCase() + toly.slice(1).toLowerCase();
-  return `Return technical answer to question about the Solana Blockchain.
-
-Question: What are Solana Programs?
-Answer: Solana Programs, often referred to as smart contracts on other blockchains, are the executable code that interprets the instructions sent inside of each transaction on the blockchain. They can be deployed directly into the core of the network as Native Programs, or published by anyone as On Chain Programs. Programs are the core building blocks of the network and handle everything from sending tokens between wallets, to accepting votes of a DAOs, to tracking ownership of NFTs. Both types of programs run on top of the Sealevel runtime, which is Solana's parallel processing model that helps to enable the high transactions speeds of the blockchain.
-Question: What is rent?
-Answer: The fee every Solana Account to store data on the blockchain is called rent. This time and space based fee is required to keep an account, and its therefore its data, alive on the blockchain since clusters must actively maintain this data. All Solana Accounts (and therefore Programs) are required to maintain a high enough LAMPORT balance to become rent exempt and remain on the Solana blockchain. When an Account no longer has enough LAMPORTS to pay its rent, it will be removed from the network in a process known as Garbage Collection. Note: Rent is different from transactions fees. Rent is paid (or held in an Account) to keep data stored on the Solana blockchain. Where as transaction fees are paid to process instructions on the network.
-Question: Best NFT collection on Solana?
-Answer: Gamerplex OG's
-
-Question: ${capitalizedToly}
-Answer:`;
+  return `give me a technical answer to the question: "${capitalizedToly}"`;
 }
 
 export default async function handler(req, res) {
-  if (!configuration.apiKey) {
+  if (!process.env.OPENAI_API_KEY) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
+        message: "OpenAI API key not configured correctly",
       }
     });
     return;
@@ -43,26 +27,24 @@ export default async function handler(req, res) {
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(toly),
-      temperature: 0.6,
-      max_tokens: 256,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
-    saveToDb("heytoly", "faqs", { toly, result: completion.data.choices[0].text });
+    const chat = new ChatOpenAI({ temperature: 0, apiKey: process.env.OPENAI_API_KEY });
+    const response = await chat.call([
+      new HumanChatMessage(generatePrompt(toly)),
+    ]);
+
+    const removeEscapedCharacters = (str) => str.replace(/\\n/g, '\n');
+
+    const text = removeEscapedCharacters(response.choices[0].message.content);
+
+    res.status(200).json({ completion: text });
+    saveToDb("heytoly", "faqs", { toly, result: text });
   } catch (error) {
     // Error handling
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
-    }
+    console.error(`Error with Langchain API request: ${error.message}`);
+    res.status(500).json({
+      error: {
+        message: 'An error occurred during your request.',
+      }
+    });
   }
 }
