5084ce8f0096402c7977817f6699681e8c21e3b1
johnforfar <john.forfar@gmail.com>
2023-05-18 16:50:38 +1000

    Upgraded langchain and added more API call error handling

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,62 +1,83 @@
-import { ChatOpenAI, HumanChatMessage } from "langchain";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
 import saveToDb from "./saveToDb.js";
 
 function generatePrompt(toly) {
-  const capitalizedToly = toly[0].toUpperCase() + toly.slice(1).toLowerCase();
-  return `give me a technical answer to the question: "${capitalizedToly}"`;
+ const capitalizedToly = toly[0].toUpperCase() + toly.slice(1).toLowerCase();
+ return `give me a technical answer to the question: "${capitalizedToly}"`;
 }
 
 export default async function handler(req, res) {
-  if (!process.env.OPENAI_API_KEY) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured correctly",
-      }
-    });
-    return;
-  }
+ if (!process.env.OPENAI_API_KEY) {
+ res.status(500).json({
+ error: {
+ message: "OpenAI API key not configured correctly",
+ }
+ });
+ return;
+ }
 
-  const toly = req.body.toly || '';
-  if (toly.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid Toly query",
-      }
-    });
-    return;
-  }
+ const toly = req.body.toly || '';
+ if (toly.trim().length === 0) {
+ res.status(400).json({
+ error: {
+ message: "Please enter a valid Toly query",
+ }
+ });
+ return;
+ }
 
-  try {
-    const chat = new ChatOpenAI({ temperature: 0, apiKey: process.env.OPENAI_API_KEY });
-    const response = await chat.call([
-      new HumanChatMessage(generatePrompt(toly)),
-    ]);
+ try {
+ const chat = new ChatOpenAI({ temperature: 0, apiKey: process.env.OPENAI_API_KEY });
+ const response = await chat.call([
+ // Add a SystemChatMessage as the first message
+ new SystemChatMessage("This is a technical FAQ chatbot that answers questions about Toly."),
+ new HumanChatMessage(generatePrompt(toly)),
+ ]);
 
-    const removeEscapedCharacters = (str) => str.replace(/\\n/g, '\n');
-    const text = removeEscapedCharacters(response.choices[0].message.content);
+ // Check if the response is valid and has choices
+ if (response && response.choices && response.choices.length > 0) {
+ // Get the first choice and its message
+ const choice = response.choices[0];
+ const { message } = choice;
+ 
+ // Check if the message is valid and has content
+ if (message && message.content) {
+ // Remove escaped characters from the content
+ const removeEscapedCharacters = (str) => str.replace(/\\n/g, '\n');
+ const text = removeEscapedCharacters(message.content);
 
-    res.status(200).json({ completion: text });
-    saveToDb("heytoly", "faqs", { toly, result: text });
-  } catch (error) {
-    // Error handling
-    console.error(`Error with Langchain API request: ${error.message}`);
+ // Send the text as the completion
+ res.status(200).json({ completion: text });
+ saveToDb("heytoly", "faqs", { toly, result: text });
+ } else {
+ // The message is invalid or has no content
+ throw new Error("The message is invalid or has no content");
+ }
+ } else {
+ // The response is invalid or has no choices
+ throw new Error("The response is invalid or has no choices");
+ }
+ } catch (error) {
+ // Error handling
+ console.error(`Error with Langchain API request: ${error.message}`);
 
-    if (error.response) {
-      // The request was made and the API responded with an error
-      console.error(`API response status: ${error.response.status}`);
-      console.error(`API response data: ${JSON.stringify(error.response.data)}`);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.error(`No response received from the API. Request details: ${error.request}`);
-    } else {
-      // Other types of errors
-      console.error(`Error message: ${error.message}`);
-    }
+ if (error.response) {
+ // The request was made and the API responded with an error
+ console.error(`API response status: ${error.response.status}`);
+ console.error(`API response data: ${JSON.stringify(error.response.data)}`);
+ } else if (error.request) {
+ // The request was made but no response was received
+ console.error(`No response received from the API. Request details: ${error.request}`);
+ } else {
+ // Other types of errors
+ console.error(`Error message: ${error.message}`);
+ }
 
-    res.status(500).json({
-      error: {
-        message: 'An error occurred during your request.',
-      },
-    });
-  }
+ res.status(500).json({
+ error: {
+ message: 'An error occurred during your request.',
+ },
+ });
+ }
 }
