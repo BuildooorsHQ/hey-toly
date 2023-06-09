1679a7898e96498752575498b794688c83f3b65f
johnforfar <john.forfar@gmail.com>
2023-06-09 19:46:51 +1000

    scraper page status added and solanaRouter fixes

--- pages/api/solanaRouter.ts
+++ pages/api/solanaRouter.ts
@@ -1,29 +1,27 @@
-import express, { Request, Response } from "express";
-import { PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { CONNECTION } from "../constants.ts";
+// ./pages/api/solanaRouter.ts
+import { NextApiRequest, NextApiResponse } from "next";
+import getBalance from "./getBalance/index.ts";
 
-const app = express();
-
-function errorHandle(
-  handler: (req: Request, res: Response<any, Record<string, any>>) => Promise<void>
+export default async function solanaRouter(
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-  return (req: Request, res: Response<any, Record<string, any>>) => {
-    handler(req, res).catch((error: Error) => {
-      console.error(error);
-      res.status(500).send({ message: "An error occurred" });
-    });
-  };
+  if (req.method !== "POST") {
+    res.status(405).end(); // Method Not Allowed
+    return;
+  }
+
+  try {
+    console.log("solanaRouter: Request received", req.body);
+
+    const balance = await getBalance(req);
+
+    console.log("solanaRouter: Balance calculated", balance);
+
+    res.status(200).json({ sol: balance });
+    return;
+  } catch (error) {
+    console.error("solanaRouter: Error occurred", error);
+    res.status(500).json({ message: "An error occurred" });
+  }
 }
-
-app.post(
-  "/getBalance",
-  errorHandle(async (req: Request, res: Response) => {
-    const { address } = req.body;
-    const balance = await CONNECTION.getBalance(new PublicKey(address));
-    res.status(200).send({ sol: balance / LAMPORTS_PER_SOL });
-  })
-);
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
